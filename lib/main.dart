2bcba330643d9ce9e18d7ea02a7859ff5efa8b62
Brian, Sirikye (CSI) <Sirikye.Brian@fao.org>
2021-03-02 13:34:07 +0300

    Adding adaptability and responsiveness to the home screens

--- lib/main.dart
+++ lib/main.dart
@@ -18,5 +18,3 @@ class WeelezaApp extends StatelessWidget {
     );
   }
 }
-
-
--- lib/screens/tabs/home_screen.dart
+++ lib/screens/tabs/home_screen.dart
@@ -17,52 +17,99 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return ListView(
             children: [
               Container(
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height / 2.5,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top:30.0,left: 20),
-                        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                          Text(
-                            "Weeleza",
-                            style: GoogleFonts.lobster(color: Colors.white, fontSize: 30),
-                          ),
-                          SizedBox(
-                            height: 5.0,
-                          ),
-                          Text(
-                            "For secure and quick delivery \nof all your items.",
-                            style: GoogleFonts.lobster(color: Colors.white, fontSize: 20),
-                          ),
-                          SizedBox(
-                            height: 50,
-                          ),
-                          Text(
-                            "Make an order with us and we will deliver \nyour items directly to "
-                                "your "
-                                "home.",
-                            style: GoogleFonts.lobster(color: Colors.white, fontSize: 15),
-                          ),
-                        ]),
-                      ),
-                      Image.asset(
-                        "assets/weeleza.png",
-                        fit: BoxFit.fill,
-                      )
-                    ],
-                  ),
+                  child: constraints.maxWidth > 600
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 30.0, left: 20),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Weeleza",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 30),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "For secure and quick delivery \nof all your items.",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    SizedBox(
+                                      height: 50,
+                                    ),
+                                    Text(
+                                      "Make an order with us and we will deliver \nyour items directly to "
+                                      "your "
+                                      "home.",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                  ]),
+                            ),
+                            Flexible(
+                              child: Image.asset(
+                                "assets/weeleza.png",
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ],
+                        )
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10.0, left: 20),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Weeleza",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "For secure and quick delivery \nof all your items.",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                    SizedBox(
+                                      height: 50,
+                                    ),
+                                    Text(
+                                      "Make an order with us and we will deliver \nyour items directly to "
+                                          "your "
+                                          "home.",
+                                      style: GoogleFonts.lobster(
+                                          color: Colors.white, fontSize: 10),
+                                    ),
+                                  ]),
+                            ),
+                            Flexible(
+                              child: Image.asset(
+                                "assets/weeleza.png",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+
+                          ],
+                        ),
                 ),
                 decoration: BoxDecoration(
                     color: Colors.deepOrange,
@@ -70,13 +117,13 @@ class HomeScreen extends StatelessWidget {
                         bottomRight: Radius.circular(50), bottomLeft: Radius.circular(50))),
               ),
               SizedBox(
-                height: 30,
+                height: 10,
               ),
               Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Quick steps to make your order.",
-                  style: GoogleFonts.robotoCondensed(fontSize: 25, fontWeight: FontWeight.bold),
+                  style: GoogleFonts.robotoCondensed(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
               SizedBox(
@@ -86,9 +133,11 @@ class HomeScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: GridView.builder(
                     shrinkWrap: true,
+                    physics: ScrollPhysics(),
                     itemCount: imageList.length,
+                    scrollDirection: Axis.vertical,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
+                      crossAxisCount: constraints.maxWidth > 600 ? 3 : 1,
                       childAspectRatio: MediaQuery.of(context).size.width /
                           (MediaQuery.of(context).size.height / 1.4),
                       mainAxisSpacing: 10.0,
@@ -98,35 +147,37 @@ class HomeScreen extends StatelessWidget {
                       return Card(
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                imageList[index],
-                                height: 120,
-                                fit: BoxFit.contain,
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                textList[index],
-                                style: GoogleFonts.robotoCondensed(
-                                    fontSize: 15,
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ],
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Image.asset(
+                                    imageList[index],
+                                    height: 120,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  textList[index],
+                                  style: GoogleFonts.robotoCondensed(
+                                      fontSize: 15,
+                                      color: Colors.black87,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
                     }),
               ),
+
             ],
-          ),
-        ),
+          );
+        },
       ),
     );
   }
