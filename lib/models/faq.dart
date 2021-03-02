7511ae3ccb03ab48ffc4c980a1aa223e3f00b122
Brian, Sirikye (CSI) <Sirikye.Brian@fao.org>
2021-03-02 17:56:20 +0300

    Adding faq model class

---
+++ lib/models/faq.dart
@@ -0,0 +1,57 @@
+class Faq {
+  String title;
+  String details;
+
+  Faq({this.title, this.details});
+}
+
+List<Faq> faqList = [
+  Faq(title: "How to book a delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to track a delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to report a wrong delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to return a wrong delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to calculate cost of a delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to get discounts on a delivery",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to create an account",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+  Faq(title: "How to package a delivery item",details: "To book a delivery, head onto our website on the "
+      "second tab click on the floating action button, fill in the details in the form and submit "
+      "your booking. "
+      "Our distribution team within a short time will work on your order and a notification "
+      "message will be sent to you with details and procedure to follow. "
+      "be sent to you the "),
+];
